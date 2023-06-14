048178ca1ea18c0e7ebae1b823d8506551f44655
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2023-06-14 10:10:48 -0300

    Add files via upload

---
+++ CadastroDeCurrículo.js
@@ -0,0 +1,19 @@
+i = 0
+if(i < 0)
+{
+    alert('Código em um arquivo externo');
+}
+else
+{
+    alert('Código em um arquivo externo 2');
+}
+while(i = 0)
+{
+    var elemento = document.getElementById("altura");
+    elemento.style.color = "red";
+    var elemento1 = documento.getElementById("site");
+    elemento.style.color = "yellow";
+    i++;
+}
+var elemento = document.getElementById("nome");;
+elemento.remove();
